acca010e79471de52a80f75d1ef9bc76c6717204
Avalander <darth.quesorico@gmail.com>
2020-02-29 07:11:13 +0100

    Remove nested timeouts.

--- playground/playground.js
+++ playground/playground.js
@@ -3,6 +3,14 @@ import {
 	render,
 } from '../src/main.js'
 
+const delay = dt => data => new Promise(resolve => {
+	setTimeout(() => resolve(data), dt)
+})
+
+const flip = fn => (...args) =>
+	fn(...args.reverse())
+
+
 const tree = h('div', [
 	'Potato',
 	h('p', { class: 'primary' }, [ 'Onion' ])
@@ -12,15 +20,20 @@ const node = document.querySelector('#app')
 
 const patch = render(node, tree)
 
-setTimeout(() => {
-	const tree_2 = h('div', [
-		h('h2', [ 'Potato' ]),
-		h('p', [ 'Onion' ]),
-	])
-	patch(tree_2, tree)
 
-	setTimeout(() => {
-		const tree_3 = h('div', [
+Promise.resolve()
+	.then(delay(1000))
+	.then(() => {
+		const tree_2 = h('div', [
+			h('h2', ['Potato']),
+			h('p', ['Onion']),
+		])
+		patch(tree_2, tree)
+		return flip(patch).bind(null, tree_2)
+	})
+	.then(delay(1000))
+	.then(_patch => {
+		const tree = h('div', [
 			h('h2', ['Ingredients']),
 			h('ul', [
 				h('li', ['Potato']),
@@ -28,36 +41,38 @@ setTimeout(() => {
 				h('li', ['Carrot']),
 			])
 		])
-		patch(tree_3, tree_2)
-
-		setTimeout(() => {
-			const tree_4 = h('div', [
-				h('h2', { class: 'primary' }, 'Ingredients'),
-				h('ul', { class: 'no-deco' }, [
-					h('li', 'Potato'),
-					h('li', 'Onion'),
-					h('li', 'Carrot'),
-				]),
-				h('button', {
-					onclick: () => console.log('Yo!'),
-				}, 'Click me!'),
-			])
-			patch(tree_4, tree_3)
-
-			setTimeout(() => {
-				const tree_5 = h('div', [
-					h('h2', { class: 'primary' }, 'Ingredients'),
-					h('ul', { class: 'no-deco' }, [
-						h('li', 'Potato'),
-						h('li', 'Onion'),
-						h('li', 'Carrot'),
-					]),
-					h('button', {
-						onclick: () => console.log('Hey!'),
-					}, 'Click me, man!'),
-				])
-				patch(tree_5, tree_4)
-			}, 1000)
-		}, 1000)
-	}, 1000)
-}, 1000)
+		_patch(tree)
+		return flip(patch).bind(null, tree)
+	})
+	.then(delay(1000))
+	.then(_patch => {
+		const tree_4 = h('div', [
+			h('h2', { class: 'primary' }, 'Ingredients'),
+			h('ul', { class: 'no-deco' }, [
+				h('li', 'Potato'),
+				h('li', 'Onion'),
+				h('li', 'Carrot'),
+			]),
+			h('button', {
+				onclick: () => console.log('Yo!'),
+			}, 'Click me!'),
+		])
+		_patch(tree_4)
+		return flip(patch).bind(null, tree_4)
+	})
+	.then(delay(1000))
+	.then(_patch => {
+		const tree_5 = h('div', [
+			h('h2', { class: 'primary' }, 'Ingredients'),
+			h('ul', { class: 'no-deco' }, [
+				h('li', 'Potato'),
+				h('li', 'Onion'),
+				h('li', 'Carrot'),
+			]),
+			h('button', {
+				onclick: () => console.log('Hey!'),
+			}, 'Click me, man!'),
+		])
+		_patch(tree_5)
+		return flip(patch).bind(null, tree_5)
+	})
